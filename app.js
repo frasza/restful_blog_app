8680e5b0f19aeb3d70a327f02f5b54554a2c02d2
Zan Fras <zan.fraas@gmail.com>
2017-10-14 12:38:47 +0200

    index route

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Imports
 const express    = require('express'),
       app        = express(),
       bodyParser = require('body-parser'),
@@ -5,6 +6,7 @@ const express    = require('express'),
 
 const PORT = 3000;
 
+// App config
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/restful_blog_app', {useMongoClient: true});
 
@@ -12,7 +14,38 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Mongoose config
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
 
+const Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
+// Index
+app.get('/blogs', (req, res) => {
+    Blog.find({})
+        .then(blogs => {
+            res.render('index', {blogs});
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+
+// App listen
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}.`);
 });
